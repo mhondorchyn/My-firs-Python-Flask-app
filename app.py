58137549ab45339c117b508ca8e20e5a9d23dc94
Myroslav <hondorchynmirko@gmail.com>
2019-07-19 13:52:12 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/test')
+def test_page():
+   return render_template ('index.html', test = True)
+
+@app.route('/')
+@app.route('/<user>')
+def default_name(user = 'Anonymus'):
+   return render_template('index.html', name = user)
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
